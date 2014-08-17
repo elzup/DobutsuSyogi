e8392b978dc5c5d5c1c3d8d73a04842dff07823a
elzzup <guild0105@gmail.com>
2014-08-18 04:51:41 +0900

    create get_game_urls methods

---
+++ get_game_url.php
@@ -0,0 +1,33 @@
+<?php
+define('REPLACE_NUM', '{NUM}');
+define('REPLACE_PLAYER', '{PLAYER}');
+define('GAME_PAR_PAGE', 10);
+define('URL_USER_HISTORY_PAGE', 'http://shogiwars.heroz.jp/users/history/' . REPLACE_PLAYER . '?gtype=a&start=' . REPLACE_NUM);
+// http://shogiwars.heroz.jp/users/history/elzup?gtype=a&start=1
+
+require_once('./lib/simple_html_dom.php');
+
+/**
+ * プレイヤー名から,そのプレイヤーへのリンクリストを取得する
+ *
+ * @param string $player_name 取得するプレイヤー名
+ * @param integer $max ゲームリンク取得数の上限
+ * @return array リンク(URL)の配列
+ */
+function get_game_urls($player_name, $max = 100) {
+    $urls = array();
+    for ($i = 1; $i < $max; $i += GAME_PAR_PAGE) {
+        $call_url = str_replace([REPLACE_NUM, REPLACE_PLAYER], [$i, $player_name], URL_USER_HISTORY_PAGE);
+        $html = file_get_html($call_url);
+        $divs = $html->find('.short_btn1');
+        if (!$divs) {
+            break;
+        }
+        foreach ($divs as $div) {
+            if ($as = $div->find('a')) {
+                $urls[] = $as[0]->href;
+            }
+        }
+    }
+    return $urls;
+}
--- get_players.php
+++ get_players.php
@@ -1,28 +1,37 @@
 <?php
-
-
 define('REPLACE_NUM', '{NUM}');
-define('PAGE_PAR_PERSON', 25);
+define('PERSON_PAR_PAGE', 25);
 define('URL_CONFERENCE', 'http://shogiwars.heroz.jp/a/events/hiyoko2?rank_criteria=max_rating&start=' . REPLACE_NUM . '&user=byeordie');
 
 require_once('./lib/simple_html_dom.php');
 
-$names = array();
-for ($i = 1; $i <= 1000; $i += PAGE_PAR_PERSON) {
-    $call_url = str_replace(REPLACE_NUM, $i, URL_CONFERENCE);
-    $html = file_get_html($call_url);
+//echo implode(',', $names);
+/**
+ * 大会の上位にランクインしているプレイヤー名を取得する
+ *
+ * @param string $url_conference コンテストページのURLフォーマット
+ * 表示する順位のはじめの値に置き換える位置にREPLACE_NUMの接続して定義しておく
+ * 'http://shogiwars.heroz.jp/a/events/hiyoko2?rank_criteria=max_rating&start=' . REPLACE_NUM . '&user=byeordie'
+ * @param integer $max プレイヤー取得数の上限
+ * @return array プレイヤー名の配列
+ */
+function get_players($url_conference, $max = 1000) {
+    $names = array();
+    for ($i = 1; $i <= $max; $i += PERSON_PAR_PAGE) {
+        $call_url = str_replace(REPLACE_NUM, $i, URL_CONFERENCE);
+        $html = file_get_html($call_url);
 
 
-    if ($table = $html->firstChild('.stripe_ranking')) {
-        foreach ($table->find('tr') as $tr) {
-            if ($as = $tr->find('a')) {
-                $href = $as[0]->href;
-                if (preg_match("#^.*/(?<name>.*)$#", $href, $m)) {
-                    $names[] = $m['name'];
+        if ($table = $html->firstChild('.stripe_ranking')) {
+            foreach ($table->find('tr') as $tr) {
+                if ($as = $tr->find('a')) {
+                    $href = $as[0]->href;
+                    if (preg_match("#^.*/(?<name>.*)$#", $href, $m)) {
+                        $names[] = $m['name'];
+                    }
                 }
             }
         }
     }
+    return $names;
 }
-
-echo implode(',', $names);
